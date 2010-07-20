00a7ed04b5645ee7d1720f66328195cd0226069c
Robert Haines <rhaines@manchester.ac.uk>
2010-07-20 09:40:32 +0100

    First cut of T2 Server Ruby API.
    Communicates with T2 Server via the REST API and provides functionality for
    running workflows and retrieving results.

---
+++ lib/t2server/run.rb
@@ -0,0 +1,228 @@
+# Copyright (c) 2010, The University of Manchester, UK.
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+#  * Neither the names of The University of Manchester nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission. 
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Robert Haines
+
+require 'rexml/document'
+include REXML
+
+module T2Server
+  
+  class Run
+    
+    STATE = {
+      :initialized => "Initialized",
+      :running     => "Operating",
+      :finished    => "Finished",
+      :stopped     => "Stopped"
+    }
+    
+    private_class_method :new
+    attr_reader :uuid
+    
+    def initialize(server, uuid)
+      @server = server
+      @uuid = uuid
+      @workflow = ""
+      @baclava = false
+      
+      @links = get_attributes(@server.get_run_attribute(uuid, ""))
+      #@links.each {|key, val| puts "#{key}: #{val}"}
+    end
+
+    def Run.create(server, workflow, uuid="")
+      if server.class == String
+        server = Server.connect(server)
+      end
+      if uuid == ""
+        new(server, server.initialize_run(workflow))
+      else
+        new(server, uuid)
+      end
+    end
+    
+    def delete
+      @server.delete_run uuid
+    end
+    
+    def inputs
+      @links[:inputs]
+    end
+    
+    def set_input(input, value)
+      @server.set_run_input(self, input, value)
+    end
+    
+    def set_input_file(input, filename)
+      @server.set_run_input_file(self, input, filename)
+    end
+    
+    def get_output(output, type="text/plain")
+      return unless finished?   ### raise exception?
+      doc = @server.get_run_attribute(@uuid, "#{@links[:wdir]}/out/#{output}")
+      doc
+    end
+    
+    def expiry
+      @server.get_run_attribute(@uuid, @links[:expiry])
+    end
+    
+    def expiry=(date)
+      @server.set_run_attribute(@uuid, @links[:expiry], date)
+    end
+
+    def workflow
+      if @workflow == ""
+        @workflow = @server.get_run_attribute(@uuid, @links[:workflow])
+      end
+      @workflow
+    end
+    
+    def status
+      @server.get_run_attribute(@uuid, @links[:status])
+    end
+    
+    def start
+      @server.set_run_attribute(@uuid, @links[:status], STATE[:running])
+    end
+    
+    def start_and_wait(params={})
+      interval = params[:interval] || 1
+      progress = params[:progress] || false
+      keepalive = params[:keepalive] || false ### TODO maybe move out of params
+      
+      # start run and wait
+      start
+      until finished?
+        sleep(interval)
+        if progress
+          print "."
+          STDOUT.flush
+        end
+      end
+      
+      # tidy up output if there is any
+      puts if progress
+    end
+    
+    def exitcode
+      @server.get_run_attribute(@uuid, @links[:exitcode]).to_i
+    end
+    
+    def stdout
+      @server.get_run_attribute(@uuid, @links[:stdout])
+    end
+    
+    def stderr
+      @server.get_run_attribute(@uuid, @links[:stderr])
+    end
+    
+    def mkdir(dir)
+      @server.make_run_dir(@uuid, @links[:wdir], dir)
+    end
+    
+    def upload_file(filename, params={})
+      location = params[:dir] || ""
+      location = "#{@links[:wdir]}/#{location}"
+      rename = params[:rename] || ""
+      @server.upload_run_file(@uuid, filename, location, rename)
+    end
+    
+    def upload_input_file(input, filename, params={})
+      file = upload_file(filename, params)
+      set_input_file(input, file)
+    end
+    
+    def upload_baclava_file(filename)
+      @baclava = true
+      rename = upload_file(filename)
+      @server.set_run_attribute(@uuid, @links[:baclava], rename)
+    end
+
+    def ls(dir="")
+      dir_list = @server.get_run_attribute(@uuid, "#{@links[:wdir]}/#{dir}")
+      doc = Document.new(dir_list)
+
+      # compile a list of directory entries stripping the
+      # directory name from the front of each filename
+      entries = []
+      XPath.each(doc, "//nss:dir", Namespaces::MAP) {|e| entries << "#{e.text.split('/')[-1]}/"}
+      XPath.each(doc, "//nss:file", Namespaces::MAP) {|e| entries << e.text.split('/')[-1]}
+      entries
+    end
+
+    def initialized?
+      status == STATE[:initialized]
+    end
+    
+    def running?
+      status == STATE[:running]
+    end
+    
+    def finished?
+      status == STATE[:finished]
+    end
+
+    private
+    def get_attributes(desc)
+      # first parse out the basic stuff
+      links = parse_description(desc)
+      
+      # get inputs
+      inputs = @server.get_run_attribute(@uuid, links[:inputs])
+      doc = Document.new(inputs)
+      nsmap = Namespaces::MAP
+      links[:baclava] = "#{links[:inputs]}/" + XPath.first(doc, "//nsr:baclava", nsmap).attributes["href"].split('/')[-1]
+
+      # set io properties
+      links[:io]       = "#{links[:listeners]}/io"
+      links[:stdout]   = "#{links[:io]}/properties/stdout"
+      links[:stderr]   = "#{links[:io]}/properties/stderr"
+      links[:exitcode] = "#{links[:io]}/properties/exitcode"
+      
+      links
+    end
+
+    def parse_description(desc)
+      doc = Document.new(desc)
+      nsmap = Namespaces::MAP
+      {
+        :expiry    => XPath.first(doc, "//nsr:expiry", nsmap).attributes["href"].split('/')[-1],
+        :workflow  => XPath.first(doc, "//nsr:creationWorkflow", nsmap).attributes["href"].split('/')[-1],
+        :status    => XPath.first(doc, "//nsr:status", nsmap).attributes["href"].split('/')[-1],
+        :wdir      => XPath.first(doc, "//nsr:workingDirectory", nsmap).attributes["href"].split('/')[-1],
+        :inputs    => XPath.first(doc, "//nsr:inputs", nsmap).attributes["href"].split('/')[-1],
+        :output    => XPath.first(doc, "//nsr:output", nsmap).attributes["href"].split('/')[-1],
+        :securectx => XPath.first(doc, "//nsr:securityContext", nsmap).attributes["href"].split('/')[-1],
+        :listeners => XPath.first(doc, "//nsr:listeners", nsmap).attributes["href"].split('/')[-1]
+      }
+    end
+  end
+end
---
+++ lib/t2server/server.rb
@@ -0,0 +1,324 @@
+# Copyright (c) 2010, The University of Manchester, UK.
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+#  * Neither the names of The University of Manchester nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission. 
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Robert Haines
+
+require 'base64'
+require 'uri'
+require 'net/http'
+require 'rexml/document'
+include REXML
+
+module T2Server
+  class Server
+    private_class_method :new
+    attr_reader :uri, :run_limit
+    
+    # list of servers we know about
+    @@servers = []
+    
+    def initialize(uri)
+      @uri = uri
+      uri = URI.parse(uri)
+      @host = uri.host
+      @port = uri.port
+      @base_path = uri.path
+      @rest_path = uri.path + "/rest"
+      @links = get_server_description
+      #@links.each {|key, val| puts "#{key}: #{val}"}
+      
+      # get max runs
+      @run_limit = get_attribute(@links[:runlimit]).to_i
+      
+      # initialise run list
+      @runs = {}
+      @runs = runs
+    end
+
+    def Server.connect(uri)
+      # see if we've already got this server
+      server = @@servers.find {|s| s.uri == uri}
+
+      if !server
+        # no, so create new one and return it
+        server = new(uri)
+        @@servers << server
+      end
+      
+      server
+    end
+    
+    def create_run(workflow)
+      uuid = initialize_run(workflow)
+      @runs[uuid] = Run.create(self, "", uuid)
+    end
+    
+    def initialize_run(workflow)
+      request = Net::HTTP::Post.new("#{@links[:runs]}")
+      request.content_type = "application/xml"
+      response = Net::HTTP.new(@host, @port).start do |http|
+        http.request(request, Fragments::WORKFLOW % workflow)
+      end
+      
+      case response
+      when Net::HTTPCreated
+        # return the uuid of the newly created run
+        epr = URI.parse(response['location'])
+        epr.path[-36..-1]
+      when Net::HTTPForbidden
+        puts "Sorry, but the server is already running its configured limit of concurrent workflows."
+        puts "Please try again later."
+        ""
+      else
+        response_error(response)
+        ""
+      end
+    end
+      
+    def runs
+      request = Net::HTTP::Get.new("#{@links[:runs]}")
+      response = Net::HTTP.new(@host, @port).start {|http| http.request(request)}
+      
+      case response
+      when Net::HTTPOK  
+        doc = Document.new(response.body)
+        
+        # get list of run uuids
+        uuids = []
+        XPath.each(doc, "//nsr:run", Namespaces::MAP) do |run|
+          uuids << run.attributes["href"].split('/')[-1]
+        end
+                
+        # add new runs
+        uuids.each do |uuid|
+          if !@runs.has_key? uuid
+            description = get_run_description(uuid)
+            @runs[uuid] = Run.create(self, "", uuid)
+          end
+        end
+        
+        # clear out the expired runs
+        if @runs.length > @run_limit
+          @runs.delete_if {|key, val| !uuids.member? key}
+        end
+
+        @runs
+      else
+        response_error(response)
+      end
+    end
+
+    def delete_run(uuid)
+      request = Net::HTTP::Delete.new("#{@links[:runs]}/#{uuid}")
+      response = Net::HTTP.new(@host, @port).start {|http| http.request(request)}
+      
+      case response
+      when Net::HTTPNoContent
+        # Success, carry on...
+        @runs.delete(uuid)
+        true
+      when Net::HTTPNotFound
+        puts "Cannot find run #{run.uuid}."
+        false
+      else
+        response_error(response)
+      end
+    end
+    
+    def delete_all_runs
+      # first refresh run list
+      runs.each_value {|run| run.delete}
+    end
+    
+    def set_run_input(run, input, value)
+      request = Net::HTTP::Put.new("#{@links[:runs]}/#{run.uuid}/#{run.inputs}/input/#{input}")
+      request.content_type = "application/xml"
+      response = Net::HTTP.new(@host, @port).start do |http|
+        http.request(request, Fragments::RUNINPUTVALUE % value)
+      end
+      
+      case response
+      when Net::HTTPOK
+        # Yay!
+        true
+      else
+        response_error(response)
+      end
+    end
+
+    def set_run_input_file(run, input, filename)
+      request = Net::HTTP::Put.new("#{@links[:runs]}/#{run.uuid}/#{run.inputs}/input/#{input}")
+      request.content_type = "application/xml"
+      response = Net::HTTP.new(@host, @port).start do |http|
+        http.request(request, Fragments::RUNINPUTFILE % filename)
+      end
+      
+      case response
+      when Net::HTTPOK
+        # Yay!
+        true
+      else
+        response_error(response)
+      end
+    end
+
+    def make_run_dir(uuid, root, dir)
+      request = Net::HTTP::Post.new("#{@links[:runs]}/#{uuid}/#{root}")
+      request.content_type = "application/xml"
+      response = Net::HTTP.new(@host, @port).start do |http|
+        http.request(request,  Fragments::MKDIR % dir)
+      end
+      
+      case response
+      when Net::HTTPCreated
+        # OK, carry on...
+        true
+      when Net::HTTPForbidden
+        puts "Error!", response.body
+        false
+      when Net::HTTPNotFound
+        puts "Cannot find run #{uuid}."
+        false
+      else
+        response_error(response)
+      end
+    end
+    
+    def upload_run_file(uuid, filename, location, rename)
+      contents = Base64.encode64(IO.read(filename))
+      rename = filename.split('/')[-1] if rename == ""
+      request = Net::HTTP::Post.new("#{@links[:runs]}/#{uuid}/#{location}")
+      request.content_type = "application/xml"
+      response = Net::HTTP.new(@host, @port).start do |http|
+        http.request(request,  Fragments::UPLOAD % [rename, contents])
+      end
+      
+      case response
+      when Net::HTTPCreated
+        # Success, return remote name of uploaded file
+        rename
+      when Net::HTTPForbidden
+        puts "Error!", response.body
+      else
+        response_error(response)
+      end
+    end
+
+    def get_run_attribute(uuid, path)
+      get_attribute("#{@links[:runs]}/#{uuid}/#{path}")
+    end
+
+    def set_run_attribute(uuid, path, value)
+      request = Net::HTTP::Put.new("#{@links[:runs]}/#{uuid}/#{path}")
+      request.content_type = "text/plain"
+      response = Net::HTTP.new(@host, @port).start {|http| http.request(request, value)}
+      
+      case response
+      when Net::HTTPOK
+        # OK, so carry on
+        true
+      when Net::HTTPForbidden
+        puts "Error!"
+        puts response.body
+        false
+      when Net::HTTPNotFound
+        puts "Cannot find run #{uuid}."
+        false
+      else
+        response_error(response)
+      end
+    end
+    
+    protected
+    def get_attribute(path)
+      request = Net::HTTP::Get.new(path)
+      response = Net::HTTP.new(@host, @port).start {|http| http.request(request)}
+      
+      case response
+      when Net::HTTPOK
+        return response.body
+      when Net::HTTPNotFound
+        puts "Cannot find attribute #{path}."
+      else
+        response_error(response)
+      end
+    end
+    
+    private
+    def get_server_description
+      request = Net::HTTP::Get.new(@rest_path)
+      response = Net::HTTP.new(@host, @port).start {|http| http.request(request)}
+      
+      case response
+      when Net::HTTPOK
+        parse_description(response.body)
+      else
+        response_error(response)
+      end
+    end
+
+    def parse_description(desc)
+      doc = Document.new(desc)
+      nsmap = Namespaces::MAP
+      {
+        :runs          => URI.parse(XPath.first(doc, "//nsr:runs", nsmap).attributes["href"]).path,
+        :runlimit      => URI.parse(XPath.first(doc, "//nsr:runLimit", nsmap).attributes["href"]).path,
+        :permworkflows => URI.parse(XPath.first(doc, "//nsr:permittedWorkflows", nsmap).attributes["href"]).path,
+        :permlisteners => URI.parse(XPath.first(doc, "//nsr:permittedListeners", nsmap).attributes["href"]).path
+      }
+    end
+
+    def get_run_description(uuid)
+      request = Net::HTTP::Get.new("#{@links[:runs]}/#{uuid}")
+      response = Net::HTTP.new(@host, @port).start {|http| http.request(request)}
+      
+      case response
+      when Net::HTTPOK
+        return response.body
+      when Net::HTTPNotFound
+        puts "Cannot find run #{uuid}."
+      else
+        response_error(response)
+      end
+    end
+    
+    def response_error(response)
+      puts "Unnexpected response from Taverna Server!"
+      puts "Server is: #{@host}:#{@port}#{@base_path}"
+      puts "Response code is: #{response.code}"
+      if response.body
+        puts "Response body is: \n#{response.body}"
+      end
+      puts "\nRaw error is: \n#{response.error!}"
+      false
+    end
+    
+  end  
+end
