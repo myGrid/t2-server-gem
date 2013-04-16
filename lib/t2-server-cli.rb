36fff1c3691ac191a2bfc18c2a14c0135d715626
Robert Haines <rhaines@manchester.ac.uk>
2013-04-16 15:24:47 +0100

    Add SSL version support to the command line tools.
    To continue to mirror curl options for the SSL parameters the different SSL
    versions can be selected with the same switches: -1, -2 or -3.

--- lib/t2-server-cli.rb
+++ lib/t2-server-cli.rb
@@ -71,6 +71,18 @@ module T2Server
           "verification.") do
             conn_params[:verify_peer] = false
         end
+        opt.on("-1", "--tlsv1", "Use TLS version 1 when negotiating with " +
+          "the remote Taverna Server server.") do
+            conn_params[:ssl_version] = :TLSv1
+        end
+        opt.on("-2", "--sslv2", "Use SSL version 2 when negotiating with " +
+          "the remote Taverna Server server.") do
+            conn_params[:ssl_version] = :SSLv23
+        end
+        opt.on("-3", "--sslv3", "Use SSL version 3 when negotiating with " +
+          "the remote Taverna Server server.") do
+            conn_params[:ssl_version] = :SSLv3
+        end
 
         # common options
         opt.on_tail("-u", "--username=USERNAME", "The username to use for " +
